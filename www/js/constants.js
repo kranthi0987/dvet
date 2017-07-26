b1145ca0d3e0cae27facdbb17b057b473104a12d
sanjay kranthi <kranthi0987@gmail.com>
2017-07-26 16:14:02 +0530

    new logo

--- www/js/constants.js
+++ www/js/constants.js
@@ -1,6 +1,6 @@
-// var SERVER_URL_HEAD = "http://dvetuatwebx1.azurewebsites.net";
+var SERVER_URL_HEAD = "http://dvetuatwebx1.azurewebsites.net";
 
-var SERVER_URL_HEAD = "http://admission.dvet.gov.in";
+// var SERVER_URL_HEAD = "http://admission.dvet.gov.in";
 
 //=201707A425656=Mynameis406@
 
